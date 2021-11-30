0d84f5e344f0b471a05a6120f25575b9aa53367f
Nezertiam <mawyn.nhek@gmail.com>
2021-11-30 19:17:17 +0100

    [ADD] Proposition model

---
+++ ccisurvey.data/Repositories/PropositionRepository.cs
@@ -0,0 +1,32 @@
+﻿using ccisurvey.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccisurvey.data.Repositories
+{
+	internal class PropositionRepository : IPropositionRepository
+	{
+		public Task AddAsync(Proposition proposition)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Proposition>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Proposition> GetAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateAsync(Proposition proposition)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

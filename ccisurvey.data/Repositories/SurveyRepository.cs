25bcd113a4c8ef86094eb14f624260935de2312d
Nezertiam <mawyn.nhek@gmail.com>
2021-12-20 20:46:14 +0100

    [ADD] Survey Repository

---
+++ ccisurvey.data/Repositories/SurveyRepository.cs
@@ -0,0 +1,52 @@
+﻿using ccisurvey.data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccisurvey.data.Repositories
+{
+	public class SurveyRepository : ISurveyRepository
+	{
+		private AppDBContext _db;
+		public SurveyRepository(AppDBContext db)
+		{
+			_db = db;
+		}
+
+		public async Task AddAsync(Survey survey)
+		{
+			await _db.Survey.AddAsync(survey);
+			return;
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			Survey survey = await _db.Survey.FindAsync(id);
+			_db.Survey.Remove(survey);
+			await _db.SaveChangesAsync();
+			return;
+		}
+
+		public async Task<List<Survey>> GetAllAsync()
+		{
+			return await _db.Survey
+				.Include(survey => survey.Propositions)
+				.ToListAsync();
+		}
+
+		public async Task<Survey> GetAsync(int id)
+		{
+			return await _db.Survey.FindAsync(id);
+		}
+
+		public async Task UpdateAsync(Survey survey)
+		{
+			_db.Survey.Update(survey);
+			await _db.SaveChangesAsync();
+			return;
+		}
+	}
+}

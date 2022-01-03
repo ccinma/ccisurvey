2e94fa60ca2e32accacef0fcd7d3877a1243c3ba
Nezertiam <mawyn.nhek@gmail.com>
2022-01-03 15:28:41 +0100

    [ADD] New survey form

---
+++ ccisurvey/Controllers/SurveyController.cs
@@ -0,0 +1,43 @@
+﻿using ccisurvey.data.Models;
+using ccisurvey.services.VMs;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ccisurvey.Controllers
+{
+	public class SurveyController : Controller
+	{
+		public async Task<IActionResult> Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(CreateSurveyViewModel model)
+		{
+			var errors = new List<string>();
+
+			if (ModelState.IsValid)
+			{
+				var survey = new Survey()
+				{
+					Label = model.Label,
+					Description = model.Description,
+					IsMultipleChoice = model.IsMultipleChoice
+				};
+
+				if (survey.Description != null)
+				{
+
+				}
+			} else
+			{
+				errors.Add("Tous les champs sont obligatoires.");
+			}
+
+			ViewData["Errors"] = errors;
+			return View();
+		}
+	}
+}

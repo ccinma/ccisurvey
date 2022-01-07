38b1faecf8f6b1f959c1b8835f644bd27af9fde6
YohanCci <alss-cdaalt21-byo@ccicampus.fr>
2022-01-07 11:30:32 +0100

    [ADD] css in create survey

--- ccisurvey.data/Migrations/20220106181810_initial.Designer.cs
+++ ccisurvey.data/Migrations/20220106181810_initial.Designer.cs
@@ -10,7 +10,7 @@ using ccisurvey.data;
 namespace ccisurvey.data.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    [Migration("20220106160946_initial")]
+    [Migration("20220106181810_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)

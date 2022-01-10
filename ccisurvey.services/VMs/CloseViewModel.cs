897d2ff44e06ee722506c13bdd2133ba3f74b287
Nezertiam <mawyn.nhek@gmail.com>
2022-01-10 14:59:12 +0100

    [ADD] Close survey

---
+++ ccisurvey.services/VMs/CloseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccisurvey.services.VMs
+{
+	public class CloseViewModel
+	{
+		public bool Close { get; set; }
+	}
+}
